bcaf06382a271a4e13195c8829d5be5c3228f6c0
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2023-12-27 10:56:47 +0530

    integrated mongodb

--- app.js
+++ app.js
@@ -1,41 +1,74 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const app = express()
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static("public"))
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs")
+const URI = process.env.MONGODB_URI
 
-app.set('view engine', 'ejs')
+async function main() {
+  mongoose.connect(URI);
 
-let expenses = []
-let expense = ""
-let places = []
-let place = ""
-let times = []
-let time = ""
+  const expenseSchema = {
+    expense: {
+      type: String,
+      required: true,
+    },
+    place: String,
+    time: String,
+  };
 
-let date = new Date().toLocaleDateString("en-IN", {
+  const Expense = mongoose.model("Expense", expenseSchema);
+
+  let date = new Date().toLocaleDateString("en-IN", {
     weekday: "long",
     month: "long",
     day: "numeric",
-    year: "numeric"
-})
+    year: "numeric",
+  });
 
+  app.get("/", async (req, res) => {
+    const items = await Expense.find()
+    if (items.length === 0) {
+        const intro = new Expense({
+          expense: "Track",
+          place: "Expense",
+          time: "Your Pocket"
+        })
+        await intro.save()
+        res.redirect("/")
+    } else {
+        res.render("list", {
+          currDate: date,
+          kharcha: items,
+        });
+    }
+  });
 
-app.get("/", (req, res) => {
-    res.render("list", {currDate: date, kharcha: expenses, jagah : places, currTimes: times})
-})
+  app.post("/", (req, res) => {
+    const expenseEntered = req.body.kharcha;
+    const placeEntered = req.body.jagah;
+    const timeEntered = new Date().toLocaleTimeString();
+    const ex = new Expense({
+      expense: expenseEntered,
+      place: placeEntered,
+      time: timeEntered,
+    })
+    ex.save()
+    res.redirect("/");
+  });
 
-app.post("/", (req, res) => {
-    expense = req.body.kharcha
-    expenses.push(expense)
-    place = req.body.jagah
-    places.push(place)
-    time = new Date().toLocaleTimeString()
-    times.push(time)
-    res.redirect("/")
-})
+  app.post("/delete", async (req, res) => {
+    const checkedItemId = req.body.checkbox;
+    await Expense.findByIdAndDelete(checkedItemId)
+    res.redirect("/");
+  })
 
+}
 app.listen(1234, () => {
-    console.log("Server running on port http://localhost:1234")
-})
+  console.log("Server running on port http://localhost:1234");
+});
+main()
